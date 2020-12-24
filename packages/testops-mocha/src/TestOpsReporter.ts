50d8189b672ac2ca2c0e20c8d03570e6cb1e7b5a
anhqle@kms-technology.com <anhqle@kms-technology.com>
2020-12-24 11:31:52 +0700

    refactor to monorepo with lerna

---
+++ packages/testops-mocha/src/TestOpsReporter.ts
@@ -0,0 +1,106 @@
+import {
+    TestResult,
+    TestSuite,
+    Metadata,
+    Status,
+    ReportLifecycle,
+    TestOpsConfiguration,
+} from '@katalon/testops-commons'
+import { v4 as uuidv4 } from 'uuid';
+import Mocha from 'mocha'
+
+export class TestOpsReporter {
+
+    private report: ReportLifecycle;
+
+    constructor() {
+        const configurationParams: TestOpsConfiguration = {
+            username: "quile@kms-technology.com",
+            password: "12345678",
+            basePath: "http://localhost:8443",
+            projectId: 5,
+            reportFolder: "./testops-result"
+        };
+        this.report = new ReportLifecycle(configurationParams);
+    }
+
+    public createMetadata(): Metadata {
+        const metadata = {
+          framework: "mocha",
+          language: "js"
+        }
+        return metadata
+    }
+
+    public onExecutionStart(): void {
+        this.report.startExecution();
+        const metadata: Metadata = this.createMetadata();
+        this.report.writeMetadata(metadata);
+    }
+
+    public onExecutionFinish(): void {
+        this.report.stopExecution();
+        this.report.writeTestResultsReport();
+        this.report.writeTestSuitesReport();
+        this.report.writeExecutionReport();
+        this.report.upload();
+    }
+
+    public onSuiteStart(suite: any): void {
+        const suiteName = suite.fullTitle();
+        if (suiteName) {
+            const suiteId: string = uuidv4();
+            suite.TO_UUID = suiteId;
+
+            const currentSuite = {} as TestSuite;
+            currentSuite.uuid = suiteId;
+            currentSuite.name = suiteName;
+            this.report.startSuite(currentSuite);
+        }
+    }
+
+    public onSuiteFinish(suite: any): void {
+        if (suite.TO_UUID) {
+          this.report.stopTestSuite(suite.TO_UUID);
+        }
+    }
+
+    public onTestStart(test: any): void {
+        test.TO_START = Date.now();
+    }
+
+    public onTestSuccess(test: Mocha.Test): void {
+        const result: TestResult = this.createTestResult(test);
+        this.endTest(result, Status.PASSED);
+    }
+
+    public onTestFailure(test: Mocha.Test, error: Error): void {
+        const result: TestResult = this.createTestResult(test);
+        result.errorMessage = error.message;
+        result.stackTrace = error.stack;
+        this.endTest(result, Status.FAILED);
+    }
+
+    public onTestPending(test: Mocha.Test): void {
+        const result: TestResult = this.createTestResult(test);
+        this.endTest(result, Status.SKIPPED);
+    }
+
+    public createTestResult(test: any): TestResult {
+        const suite: any = test.parent;
+        const result = { } as TestResult;
+        result.name = test.title;
+        result.uuid = uuidv4();
+        result.start = test.TO_START;
+        result.duration = test.duration;
+        result.suiteName = suite.fullTitle();
+        return result;
+    }
+
+    private endTest(result: any, status: Status): void {
+        result.status = status;
+        result.stop = Date.now();
+        result.duration = result.stop - result.start;
+        this.report.stopTestCase(result);
+    }
+}
