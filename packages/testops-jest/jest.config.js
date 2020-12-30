a801b802144a5bba99be9fdefe1bf46ecb3de071
anhqle@kms-technology.com <anhqle@kms-technology.com>
2020-12-30 13:37:23 +0700

    update

--- packages/testops-jest/jest.config.js
+++ packages/testops-jest/jest.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  reporters: ['default',
-    ['.',
-      {
-        username: "anhle@mailinator.com",
-        password: "12345678",
-        basePath: "http://localhost:8444",
-        projectId: 241,
-        reportFolder: "./testops-result",
-      }],
-  ]
+  reporters: ['default', '.']
 }
